a3f6b76d0adb9192c0effd7b170dfd7ce1196097
Daniel / Pavlos <135515773+Pavlostheone@users.noreply.github.com>
2025-07-05 21:46:08 +0200

    Update edit-book.ts

--- frontend/book-quotes/src/app/edit-book/edit-book.ts
+++ frontend/book-quotes/src/app/edit-book/edit-book.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-edit-book',
-  imports: [],
   templateUrl: './edit-book.html',
-  styleUrl: './edit-book.scss'
+  imports: [CommonModule, FormsModule],
 })
 export class EditBook {
+  books: any[] = [];
+  editBookId: number | null = null;
+  editTitle = '';
+  editAuthor = '';
 
+  startEdit(book: any) {
+    this.editBookId = book.id;
+    this.editTitle = book.title;
+    this.editAuthor = book.author;
+  }
+
+  saveEdit(id: number) {
+    const book = this.books.find(b => b.id === id);
+    if (book) {
+      book.title = this.editTitle;
+      book.author = this.editAuthor;
+      this.cancelEdit();
+    }
+  }
+
+  cancelEdit() {
+    this.editBookId = null;
+    this.editTitle = '';
+    this.editAuthor = '';
+  }
+
+  deleteBook(id: number) {
+    this.books = this.books.filter(b => b.id !== id);
+  }
 }
