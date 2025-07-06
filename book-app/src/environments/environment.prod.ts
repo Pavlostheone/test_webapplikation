27f599b361e33d7e0fd302e094f48260aba5de2e
Pavlostheone <danielmironidis@gmail.com>
2025-07-06 21:50:58 +0200

    remove /api url

--- book-app/src/environments/environment.prod.ts
+++ book-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://mybookapi-beafd4dtf4gbg3eg.westeurope-01.azurewebsites.net/api'  // replace with your deployed API URL
+  apiBaseUrl: 'https://mybookapi-beafd4dtf4gbg3eg.westeurope-01.azurewebsites.net'  // replace with your deployed API URL
 };
