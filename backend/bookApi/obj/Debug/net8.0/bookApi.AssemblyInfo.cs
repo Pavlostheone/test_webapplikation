8d3c79cec547bd20b20f6b02218f7fdf4e68d9f6
Pavlostheone <danielmironidis@gmail.com>
2025-07-08 13:30:23 +0200

    add src _redirects, and update project structure

--- backend/bookApi/obj/Debug/net8.0/bookApi.AssemblyInfo.cs
+++ backend/bookApi/obj/Debug/net8.0/bookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d587831f4f1e53c03af87d8f0799c30ae98a662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5368f50696dc92f02d7a1463f6d69c72512f6765")]
 [assembly: System.Reflection.AssemblyProductAttribute("bookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
